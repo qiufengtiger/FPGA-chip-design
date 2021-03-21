23ef4276c901bfc2c6c2db2dba68c0b6aea67206
qiufengtiger <zfengr@me.com>
2021-03-21 12:01:02 -0400

    CB connection fixed

--- src/connection_block.v
+++ src/connection_block.v
@@ -14,7 +14,7 @@ module connection_block(clk, tracks_0, tracks_1, out_0, out_1, scan_in, scan_out
 	// <- 0_0
 	// -> 1_0
 	// <- 0_1
-	// -> 1_0 etc
+	// -> 1_1 etc
 	// refer to this slide for CB block diagram
 	// https://docs.google.com/presentation/d/1zSjzy-MxLvTViwhgN_G6zG-wbjkTXp0XCrzEKIEBBog/edit#slide=id.gc5f0a1aeb8_1_27
 	input [CHANNEL_ONEWAY_WIDTH-1:0] tracks_0, tracks_1;
@@ -26,7 +26,7 @@ module connection_block(clk, tracks_0, tracks_1, out_0, out_1, scan_in, scan_out
 	// each out is controlled by 1 4:1 mux, so in total 2*2 contorl bits
 	shift_reg #(4) inst_cb_out_config(.clk(clk), .out(out_config), .scan_in(scan_in), .scan_out(scan_out), .scan_en(scan_en));
 
-	mux_1bit #(4, 2) inst_cb_out_mux_0(.in(tracks_0), .select(out_config[1:0]), .out(out_0));
-	mux_1bit #(4, 2) inst_cb_out_mux_1(.in(tracks_1), .select(out_config[3:2]), .out(out_1));
+	mux_1bit #(4, 2) inst_cb_out_mux_0(.in({tracks_0[0], tracks_1[0], tracks_0[2], tracks_1[2]}), .select(out_config[1:0]), .out(out_0));
+	mux_1bit #(4, 2) inst_cb_out_mux_1(.in({tracks_0[1], tracks_1[1], tracks_0[3], tracks_1[3]}), .select(out_config[3:2]), .out(out_1));
 	
 endmodule
